781e3a0a162df3a8b9d113a65d4af37cef8641cd
freyagracia <freyagracia14@gmail.com>
2024-11-14 22:08:10 +0700

    background

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import base64
 
 st.set_page_config(
     page_title="Manajemen Jaringan Kelompok berapa",
@@ -17,11 +17,21 @@ col1, col2 = st.columns([1, 1])
 st.page_link("pages/1.py", label=" Cryptography 1", icon="1️⃣")
 st.page_link("pages/2.py", label=" Cryptography 2", icon="2️⃣")
 
-# Tombol untuk navigasi ke halaman Caesar dan Vignere
-# with col1:
-#     if st.button("Pergi ke Halaman 1"):
-#         st.experimental_set_query_params(page="caesar.py")
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-# with col2: 
-#     if st.button("Pergi ke Halaman 2"):
-#         st.experimental_set_query_params(page="vigenere.py")
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('./images/bg4.png')
